995688479c8a89eee74eeb9ae778a93158bcde2d
unknown <mgutierrez@wi-mobile.com>
2019-08-30 09:44:03 -0500

    ajustes al git

---
+++ templates_c/7b7f64d69dc24eaacf80b86c2db63d218aa2c67b_0.file.index.html.php
@@ -0,0 +1,55 @@
+<?php
+/* Smarty version 3.1.30, created on 2019-08-30 14:43:33
+  from "C:\wamp\www\uandes_prueba\templates\index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5d6936155984e2_00557957',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7b7f64d69dc24eaacf80b86c2db63d218aa2c67b' => 
+    array (
+      0 => 'C:\\wamp\\www\\uandes_prueba\\templates\\index.html',
+      1 => 1567131493,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:header.html' => 1,
+    'file:menu.html' => 1,
+    'file:slider.html' => 1,
+    'file:explora.html' => 1,
+    'file:footer.html' => 1,
+  ),
+),false)) {
+function content_5d6936155984e2_00557957 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<!-- TEMPLATE DE HEADER  -->
+<?php $_smarty_tpl->_subTemplateRender("file:header.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+
+<!-- TEMPLATE DEL MENU  -->
+<?php $_smarty_tpl->_subTemplateRender("file:menu.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+
+<!-- TEMPLATE DE SLIDER  -->
+<?php $_smarty_tpl->_subTemplateRender("file:slider.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+
+<!-- TEMPLATE DE EXPLORA   -->
+<?php $_smarty_tpl->_subTemplateRender("file:explora.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+
+<!-- TEMPLATE DE FOOTER  -->
+<?php $_smarty_tpl->_subTemplateRender("file:footer.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
+<?php }
+}
